07bf3a6b106c6caa65fa1083708c648ded69d8a7
Anton <antoni.lunenko@gmail.com>
2018-05-21 02:43:47 +0300

    Edit the customer

--- webtrekk-customers/src/app/app.module.ts
+++ webtrekk-customers/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
+=======
+import { CustomerComponent } from './customer/customer.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+>>>>>>> d32bf2b... Edit the customer
 
 @NgModule({
   declarations: [
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
