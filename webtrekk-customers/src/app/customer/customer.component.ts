02dafd8195a80cffe44f5d93affda862da70c692
Anton <antoni.lunenko@gmail.com>
2018-05-21 02:24:17 +0300

    Create a Customer class. Show the customer object. Format with the UppercasePipe.

--- webtrekk-customers/src/app/customer/customer.component.ts
+++ webtrekk-customers/src/app/customer/customer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Customer } from '../customer';
+
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
-  customer = 'Jim';
+  customer = {
+      "customerID": 1,
+      "name": {
+          "first": "Peter",
+          "last": "Smith"
+      },
+      "birthday": "1996-10-12",
+      "gender": "m",
+      "lastContact": "2017-06-01T23:28:56.782Z",
+      "customerLifetimeValue": 191.12
+  };
 
   constructor() { }
 
