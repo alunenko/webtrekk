9b5e09f64edab55cdd6bc10c72cad22296f7f974
Anton <antoni.lunenko@gmail.com>
2018-05-21 02:00:43 +0300

    init: bootstrap, module webtrekk-customers

---
+++ webtrekk-customers/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  appName = `Webtrekk customers`;
+}
