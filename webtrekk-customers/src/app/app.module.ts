9ad7f5ad6024335013a4ad51f093a9fdae43e332
Anton <antoni.lunenko@gmail.com>
2018-05-21 02:04:15 +0300

    Create the customer component

--- webtrekk-customers/src/app/app.module.ts
+++ webtrekk-customers/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
